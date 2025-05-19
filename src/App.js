af7660d7768e1c4f2f24f44098c787882e22214e
HannahNC23 <hannah.charles@boots.co.uk>
2025-05-19 08:10:52 +0100

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is coded by Hannah Nicole Charles-Murphy and is{" "}
+        <a
+          href="https://github.com/HannahNC23/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
