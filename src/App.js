70403edb798ba6210ce4298f430ea2946a9754b5
HannahNC23 <hannah.charles@boots.co.uk>
2025-05-19 08:25:52 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project is coded by Hannah Nicole Charles-Murphy and is{" "}
-        <a
-          href="https://github.com/HannahNC23/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by Hannah Nicole Charles-Murphy and is{" "}
+          <a
+            href="https://github.com/HannahNC23/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
